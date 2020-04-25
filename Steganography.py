5588136a6def699d27b0a171cd225a398719083e
Evangellos Wiegers (eaw568) <evan.wiegers@usask.ca>
2020-04-25 15:35:30 -0600

    updated steganography

--- Steganography.py
+++ Steganography.py
@@ -1,6 +1,8 @@
 # Evangellos Wiegers
 # Note: be sure to have stegano installed
 
+#zbar issues again
+
 from stegano import lsbset
 from stegano.lsbset import generators
 import inspect
@@ -16,6 +18,9 @@ def hide_text(image, message, new_image):
     :param new_image: new image name
     :return: N/A
     '''
+    assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
+    if ".png" not in new_image and ".jpg" not in new_image:
+        new_image = new_image + ".png"
     lsbset.hide(image, message, gen).save(new_image)
 
 def hide_in_spec(image, message, new_image):
@@ -26,6 +31,9 @@ def hide_in_spec(image, message, new_image):
     :param new_image: new image name
     :return: N/A
     '''
+    assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
+    if ".png" not in new_image and ".jpg" not in new_image:
+        new_image = new_image + ".png"
     exi.hide(image, new_image, message)
 
 def reveal_text(image):
@@ -34,6 +42,7 @@ def reveal_text(image):
     :param image: image with hidden text
     :return: hidden text
     '''
+    assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
     return lsbset.reveal(image, gen)
 
 def reveal_in_spec(image):
@@ -42,6 +51,7 @@ def reveal_in_spec(image):
     :param image: image with hidden text
     :return: hidden text
     '''
+    assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
     return exi.reveal(image)
 
 def get_generators():
@@ -51,3 +61,14 @@ def get_generators():
     '''
     all_generators = inspect.getmembers(generators, inspect.isfunction)
     return ["{0} - {1}".format(str(g[0]), str(g[1])) for g in all_generators]
+
+if __name__ == '__main__':
+    image1 = "superintendent1.png"
+    image2 = "superintendent2.png"
+
+    hide_text(image1, "Tamam Shud", "hidden1.png")
+    hide_in_spec(image2, "Epstein didnt kill himself", "hidden2.png")
+
+    print(get_generators())
+    print(reveal_text(image1))
+    print(reveal_in_spec(image2))
