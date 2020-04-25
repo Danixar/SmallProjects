19a9c34a3ff55dc312357dd9c160b08aabe81134
Evangellos Wiegers (eaw568) <evan.wiegers@usask.ca>
2020-04-25 15:57:14 -0600

    updated steganography testing

--- Steganography.py
+++ Steganography.py
@@ -3,12 +3,13 @@
 
 #zbar issues again
 
-from stegano import lsbset
+from stegano import lsb
 from stegano.lsbset import generators
 import inspect
 from stegano import exifHeader as exi
+from pathlib import Path
 
-gen = generators.eratosthenes() # the generator to use for hiding text not in specification
+# gen = generators.eratosthenes() # the generator to use for hiding text not in specification
 
 def hide_text(image, message, new_image):
     '''
@@ -19,9 +20,9 @@ def hide_text(image, message, new_image):
     :return: N/A
     '''
     assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
-    if ".png" not in new_image and ".jpg" not in new_image:
-        new_image = new_image + ".png"
-    lsbset.hide(image, message, gen).save(new_image)
+    my_file = Path(image)
+    if my_file.is_file():
+        lsb.hide(image, message).save(new_image)
 
 def hide_in_spec(image, message, new_image):
     '''
@@ -32,9 +33,9 @@ def hide_in_spec(image, message, new_image):
     :return: N/A
     '''
     assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
-    if ".png" not in new_image and ".jpg" not in new_image:
-        new_image = new_image + ".png"
-    exi.hide(image, new_image, message)
+    my_file = Path(image)
+    if my_file.is_file():
+        exi.hide(image, new_image, message)
 
 def reveal_text(image):
     '''
@@ -43,7 +44,7 @@ def reveal_text(image):
     :return: hidden text
     '''
     assert ".png" in image.lower() or ".jpg" in image.lower() or ".jpeg" in image.lower(), "image is wrong data type!"
-    return lsbset.reveal(image, gen)
+    return lsb.reveal(image)
 
 def reveal_in_spec(image):
     '''
@@ -60,15 +61,17 @@ def get_generators():
     :return: a string list of all available generators
     '''
     all_generators = inspect.getmembers(generators, inspect.isfunction)
-    return ["{0} - {1}".format(str(g[0]), str(g[1])) for g in all_generators]
+    return ["{0}".format(str(g[0])) for g in all_generators]
 
 if __name__ == '__main__':
     image1 = "superintendent1.png"
     image2 = "superintendent2.png"
+    new_image1 = "hidden1.png"
+    new_image2 = "hidden2.png"
 
-    hide_text(image1, "Tamam Shud", "hidden1.png")
-    hide_in_spec(image2, "Epstein didnt kill himself", "hidden2.png")
+    hide_text(image1, "Tamam Shud", new_image1)
+    hide_in_spec(image2, "Epstein didnt kill himself", new_image2)
 
     print(get_generators())
-    print(reveal_text(image1))
-    print(reveal_in_spec(image2))
+    print(reveal_text(new_image1))
+    print(reveal_in_spec(new_image2))
